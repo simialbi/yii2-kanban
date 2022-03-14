d23009caf0a2506b6b4357059c7e8279ff13f024
Simon Karlen <simi.albi@outlook.com>
2022-03-14 13:00:57 +0100

    added successful test

--- tests/functional/BoardCest.php
+++ tests/functional/BoardCest.php
@@ -7,6 +7,7 @@
 namespace simialbi\extensions\kanban\functional;
 
 use simialbi\extensions\kanban\FunctionalTester;
+use simialbi\yii2\kanban\models\Board;
 
 class BoardCest
 {
@@ -31,5 +32,25 @@ class BoardCest
         $I->amOnRoute('kanban/plan/create');
         $I->submitForm('#sa-kanban-create-plan-form', []);
         $I->see('Name darf nicht leer sein.', '.invalid-feedback');
+    }
+
+    public function submitCreateForm(FunctionalTester $I)
+    {
+        $I->amOnRoute('kanban/plan/create');
+        $I->submitForm('#sa-kanban-create-plan-form', [
+            'Board[name]' => 'Test',
+            'Board[is_public]' => 0
+        ]);
+
+        /** @var Board $board */
+        $board = $I->grabRecord(Board::class, [
+            'id' => 1
+        ]);
+        $I->assertNotNull($board);
+        $I->assertInstanceOf(Board::class, $board);
+        $I->assertEquals('Test', $board->name);
+        $I->assertEquals(1, $board->id);
+        $I->assertEquals(false, $board->is_public);
+        $I->assertNull($board->image);
     }
 }
